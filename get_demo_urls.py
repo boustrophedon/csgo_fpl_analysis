7a744a4f9d1a850f19ae112950fcb260f36d5ac5
Harry Stern <hcs@meow.sh>
2018-09-04 21:23:06 -0400

    Rename and fix demo url getter to query faceit API in chunks

--- get_demo_urls.py
+++ get_demo_urls.py
@@ -1,29 +1,37 @@
 import requests
+import time
 
 API_KEY = open(".faceit_key", "r").read().strip()
 
 NA_FPL = "748cf78c-be73-4eb9-b131-21552f2f8b75"
 EU_FPL = "74caad23-077b-4ef3-8b1d-c6a2254dfa75"
 
-# `hub` is a string containing the hub id.
+# `hub` is a string containing the hub id. `first_match` is the offset into the
+# matches provided by Faceit. `num_matches` is the number of matches to query.
 # Outputs a list of json objects containing information on the most recent
-# matches played in that hub. Defaults to 200 matches.
-def get_matches(hub, num_matches=200):
+# matches played in that hub. Defaults to first 20 matches.
+def get_matches(hub, first_match=0, num_matches=20):
+    REQUEST_STEP = 20
+
     matches_api_url = "https://open.faceit.com/data/v4/hubs/{hub}/matches" \
         .format(hub=hub)
 
-    params = {"type":"past", "offset":"0", "limit":num_matches}
 
     auth_header = "Bearer {}".format(API_KEY)
     accept_header = "application/json"
 
     headers = {"Authorization": auth_header, "Accept": accept_header}
 
-    resp = requests.get(matches_api_url, params=params, headers=headers)
+    matches = list()
+    for start in range(first_match, num_matches, REQUEST_STEP):
+        params = {"type":"past", "offset":start, "limit":REQUEST_STEP}
+        resp = requests.get(matches_api_url, params=params, headers=headers)
 
-    print("Response getting matches: {}".format(resp.status_code))
+        print("Response getting matches: {}".format(resp.status_code))
+        matches+= resp.json()['items']
+        time.sleep(1)
 
-    return resp.json()['items']
+    return matches
 
 # Given a list of match json objects, filter out incomplete ones.
 def filter_incomplete_matches(matches):
@@ -43,12 +51,18 @@ def get_demo_urls(matches):
     return demo_urls
 
 if __name__ == '__main__':
-    na_fpl_urls = get_demo_urls(filter_incomplete_matches(get_matches(NA_FPL)))
+    # because we filter out matches that did not get played, this is actually
+    # an upper bound on the number of demos
+    NUM_DEMOS = 250
+
+    na_matches = get_matches(NA_FPL, num_matches=NUM_DEMOS)
+    na_fpl_urls = get_demo_urls(filter_incomplete_matches(na_matches))
 
     with open("na_fpl_demos.txt", "a") as f:
         f.write('\n'.join(na_fpl_urls))
 
-    eu_fpl_urls = get_demo_urls(filter_incomplete_matches(get_matches(eu_FPL)))
+    eu_matches = get_matches(EU_FPL, num_matches=NUM_DEMOS)
+    eu_fpl_urls = get_demo_urls(filter_incomplete_matches(eu_matches))
 
     with open("eu_fpl_demos.txt", "a") as f:
         f.write('\n'.join(eu_fpl_urls))
